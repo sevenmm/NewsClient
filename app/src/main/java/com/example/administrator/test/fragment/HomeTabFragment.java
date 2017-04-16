cd50098ddca1031af43dad2b2cccc7e1cd06a1c1
007 <351079016@qq.com>
2017-04-16 20:38:06 +0800

    16.去掉无用包

--- app/src/main/java/com/example/administrator/test/fragment/HomeTabFragment.java
+++ app/src/main/java/com/example/administrator/test/fragment/HomeTabFragment.java
@@ -1,12 +1,5 @@
 package com.example.administrator.test.fragment;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.administrator.test.base.BaseFragment;
